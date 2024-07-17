826781686a176548f8d47908247e8e7cb17fa487
KAAN <alikaanozoll@gmail.com>
2024-07-17 22:10:22 +0300

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ali Kaan Özol");
+            Console.WriteLine("Patika'ya güvendiğimden dolayı bu bootcampteyim!");
+        }
+
+    }
+}
